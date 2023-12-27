d4634cd5b19a43c737d6509625f4dc748b0a06aa
malaaaky <malak.cheriff@gmail.com>
2023-12-27 03:00:03 +0200

    adding first models

---
+++ Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string ProfilePictureURl { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+
+    }
+}
