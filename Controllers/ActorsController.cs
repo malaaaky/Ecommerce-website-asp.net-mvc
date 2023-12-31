0a12a7b14381f74eb4852956f018e333a978fd6f
SharkEmad <shodyemad@gmail.com>
2023-12-31 09:52:04 +0200

    Now user can login and logout and can purchase any items and add to cart

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -1,8 +1,9 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Data.Static;
 using eTickets.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,26 +11,31 @@ using System.Threading.Tasks;
 
 namespace eTickets.Controllers
 {
+    [Authorize(Roles = UserRoles.Admin)]
     public class ActorsController : Controller
     {
-        //Working with Actor Data
         private readonly IActorsService _service;
 
         public ActorsController(IActorsService service)
         {
             _service = service;
         }
+
+        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
             var data = await _service.GetAllAsync();
             return View(data);
         }
+
+        //Get: Actors/Create
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")]Actor actor)
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Actor actor)
         {
             if (!ModelState.IsValid)
             {
@@ -40,23 +46,25 @@ namespace eTickets.Controllers
         }
 
         //Get: Actors/Details/1
-        public async Task<IActionResult> Details(int ID)
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int id)
         {
-            var actorDetails = await _service.GetByIdAsync(ID);
+            var actorDetails = await _service.GetByIdAsync(id);
+
             if (actorDetails == null) return View("NotFound");
             return View(actorDetails);
         }
 
-        public async Task<IActionResult> Edit(int ID)
+        //Get: Actors/Edit/1
+        public async Task<IActionResult> Edit(int id)
         {
-
-            var actorDetails = await _service.GetByIdAsync(ID);
+            var actorDetails = await _service.GetByIdAsync(id);
             if (actorDetails == null) return View("NotFound");
-
             return View(actorDetails);
         }
+
         [HttpPost]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,FullName,ProfilePictureURL,Bio")] Actor actor)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
         {
             if (!ModelState.IsValid)
             {
@@ -66,24 +74,22 @@ namespace eTickets.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public async Task<IActionResult> Delete(int ID)
+        //Get: Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
         {
-
-            var actorDetails = await _service.GetByIdAsync(ID);
+            var actorDetails = await _service.GetByIdAsync(id);
             if (actorDetails == null) return View("NotFound");
-
             return View(actorDetails);
         }
+
         [HttpPost, ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(int ID)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var actorDetails = await _service.GetByIdAsync(ID);
+            var actorDetails = await _service.GetByIdAsync(id);
             if (actorDetails == null) return View("NotFound");
 
-            await _service.DeleteAsync(ID);
+            await _service.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
         }
-
-
     }
 }
